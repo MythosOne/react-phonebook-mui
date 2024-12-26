f933eccb090569d9d2dfa15e6eb0ab712a2fbb65
MythosOne <107143884+MythosOne@users.noreply.github.com>
2024-12-26 21:10:31 +0200

    Fixed bug input password visibility, corrected page style

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -7,6 +7,6 @@ export const Container = styled.section`
   max-width: 1024px;
   min-height: calc(90vh - 50px);
 
-  border-radius: 10px;
-  box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
+  /* border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px; */
 `;
