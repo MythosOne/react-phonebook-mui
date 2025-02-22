136a49e7810ee39df65f1ef37d5a13907f0e2d31
Victor <107143884+MythosOne@users.noreply.github.com>
2025-02-22 12:57:46 +0200

    Fixing page Register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { unwrapResult } from '@reduxjs/toolkit';
@@ -24,36 +24,64 @@ const styles = {
     transform: 'translateY(0)',
     transition: 'opacity 300ms ease-out, transform 300ms ease-out',
   },
+  message: {
+    zIndex: 1001,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translateX(-50%)',
+    padding: '10px 20px',
+    fontSize: '20px',
+    color: 'white',
+    textShadow: '2px 2px 4px black',
+  },
 };
 
 const Register = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const nodeRef = useRef(null);
+  const [showMessage, setShowMessage] = useState(false);
+  const [showRegForm, setShowRegForm] = useState(true);
 
   const handleRegister = async userData => {
     try {
       const resultAction = await dispatch(register(userData));
       unwrapResult(resultAction);
-      navigate('/login');
+      setShowMessage(true);
+      setShowRegForm(false);
+      setTimeout(() => navigate('/login'), 3000);
     } catch (error) {
-      console.error('Registration failed. Please try again.', error);
+      console.error('Registration failed:', error);
     }
   };
 
   return (
     <section style={styles.container}>
-      <Transition in={true} appear={true} timeout={300} nodeRef={nodeRef}>
-        {state => (
-          <RegisterForm
-            ref={nodeRef}
-            onSubmit={handleRegister}
-            style={{
-              ...styles.initial,
-              ...(state === 'entered' && styles.entered),
-            }}
-          />
-        )}
+      {showMessage && (
+        <span style={styles.message}>
+          Registration successful! <br />
+          Please login to continue
+        </span>
+      )}
+      <Transition
+        in={showRegForm}
+        appear={true}
+        timeout={300}
+        nodeRef={nodeRef}
+      >
+        {state =>
+          showRegForm && (
+            <RegisterForm
+              ref={nodeRef}
+              onSubmit={handleRegister}
+              style={{
+                ...styles.initial,
+                ...(state === 'entered' && styles.entered),
+              }}
+            />
+          )
+        }
       </Transition>
     </section>
   );
